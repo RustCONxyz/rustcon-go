6248329ebf6086e02cd0efe43a4c0bae3999e245
Kopter <58952825+Kop7er@users.noreply.github.com>
2024-05-29 02:04:59 +0100

    Added connection test

--- rustcon_test.go
+++ rustcon_test.go
@@ -1,14 +1,32 @@
 package rustcon
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
 	"testing"
+
+	"github.com/gorilla/websocket"
 )
+
+const rconPassword = "password"
+
+var upgrader = websocket.Upgrader{}
+
+func createMockRustServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/"+rconPassword {
+			upgrader.Upgrade(w, r, nil)
+		}
+	}))
+}
 
 func TestConfig(t *testing.T) {
 	connOptions := RconConnectionOptions{
 		IP:       "127.0.0.1",
 		Port:     28016,
-		Password: "password",
+		Password: rconPassword,
 	}
 
 	conn, err := NewRconConnection(connOptions)
@@ -25,4 +43,25 @@ func TestConfig(t *testing.T) {
 	if conn.Password != connOptions.Password {
 		t.Errorf("expected password to be %s, got %s", connOptions.Password, conn.Password)
 	}
+}
+
+func TestConnection(t *testing.T) {
+	mockServer := createMockRustServer()
+	defer mockServer.Close()
+
+	parsedURL, _ := url.Parse(mockServer.URL)
+
+	serverIP := parsedURL.Hostname()
+	serverPort, _ := strconv.Atoi(parsedURL.Port())
+
+	conn := RconConnection{
+		IP:       serverIP,
+		Port:     serverPort,
+		Password: rconPassword,
+	}
+
+	if err := conn.Connect(); err != nil {
+		t.Error(err)
+	}
+	defer conn.Disconnect()
 }
